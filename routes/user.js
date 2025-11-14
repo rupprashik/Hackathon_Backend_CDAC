b73a26f2b831b6bc203933ccc094a438a47173f6
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2025-11-14 12:57:44 +0530

    backend api

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const cryptoJs = require('crypto-js')
 const router = express.Router();
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
+const result = require('../utils/result')
 router.post('/register', (req, res) => {
     const { first_name, last_name, email, password, mobile, birth } = req.body;
 
@@ -21,7 +22,7 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
-
+    console.log("Loginnnnnnn")
     const encryptedPassword = String(cryptoJs.SHA256(password))
 
     const sql = `SELECT * FROM users WHERE email = ? AND password = ?`
@@ -29,16 +30,18 @@ router.post('/login', (req, res) => {
     pool.query(sql, [email, encryptedPassword], (error, data) => {
         if (data) {
             if (data.length != 0) {
+                console.log(data)
                 const payload = {
-                    userId: data[0].id,
+                    userId: data[0].u_id,
                 }
                 const token = jwt.sign(payload, config.secret)
                 const body = {
                     token: token,
-                    firstName: data[0].firstName,
-                    lastName: data[0].lastName,
+                    firstName: data[0].first_name,
+                    lastName: data[0].last_name,
                 }
-                res.send(body)
+                console.log("at last")
+                res.send(result.createSuccessResult(body))
             } else res.send(error)
         } else res.send(error)
     })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express =require ('express');
 const userRouter = require('../routes/user');
+const cors = require('cors')
 const app = express();
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter)
 app.listen(4000,'localhost',()=>
 {
+
     console.log("Server started on 400");
 })
